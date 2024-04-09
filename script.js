27e77c29d6826b7a39da9febfabae940053123cd
Emangod <78701460+eman225511@users.noreply.github.com>
2024-04-08 22:17:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-function GetURLParameter(sParam)
-{
+function GetURLParameter(sParam) {
     var sPageURL = window.location.search.substring(1);
     var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++) 
-    {
+    for (var i = 0; i < sURLVariables.length; i++) {
         var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam) 
-        {
+        if (sParameterName[0] == sParam) {
             return sParameterName[1];
         }
     }
